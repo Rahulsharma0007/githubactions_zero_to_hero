8f24e2418832623d4a85b74c07dfbda450670f72
RahulSharma0007 <Rahul266sharma@gmail.com>
2026-03-17 03:41:12 +0000

    linting app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 
 def hello_world():
     return render_template('index.html')
@@ -11,4 +11,3 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
-    
